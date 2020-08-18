c59373899fcc0966be93c03c7783cf49b4985d9c
Grace Lindelien <gracelindelien@gmail.com>
2020-08-18 12:24:30 -0700

    Render description/amenities service on proxy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+app.use(cors());
 
-app.use(express.static('public'));
+app.get('/listing/:id', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, '../public')});
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/listing/:id/info', (req, res) => {
+  res.redirect(`http://localhost:3333/listing/${req.params.id}/info`);
+});
+
+app.get('/listing/:id/amenities', (req, res) => {
+  res.redirect(`http://localhost:3333/listing/${req.params.id}/amenities`);
+});
+
+app.get('/listing/:id/highlights', (req, res) => {
+  res.redirect(`http://localhost:3333/listing/${req.params.id}/highlights`);
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`);
 });
